33d5801ce39d37e7d4c9a11ec22bb5d06fb07b46
kmanadkat <kmanadkat@gmail.com>
2024-08-08 21:19:23 +0530

    feat: Added Postman MockServer For Dev

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.DEV ? 'https://c38fba0b-daee-4b86-840f-c2a9628b2367.mock.pstmn.io/api' : 'https://api.rawg.io/api'
+
 export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
+  baseURL: baseURL,
   params: {
     key: import.meta.env.VITE_API_KEY,
   },
