37bb361ec8c2e7ceb77249e6d31a23cb818e6df7
kmanadkat <kmanadkat@gmail.com>
2024-08-09 20:25:25 +0530

    feat: Production Ready

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.DEV ? 'https://8172bb16-7f42-4e8d-9611-37ea8a9e6dfe.mock.pstmn.io/api' : 'https://api.rawg.io/api'
-// const baseURL = 'https://api.rawg.io/api'
+const baseURL = 'https://api.rawg.io/api'
 
 export default axios.create({
   baseURL: baseURL,
